34f1099c2405647673102e8ee508ba91aae2b7bd
astevens104 <agstevens104@gmail.com>
2024-08-07 16:40:00 -0700

    finished contact js

---
+++ index.js
@@ -0,0 +1,27 @@
+// template_d0yyov9
+// service_u7j72lr
+// F2RTnso70QpihR1dI
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+        .sendForm(
+            'service_u7j72lr',
+            'template_d0yyov9',
+            event.target,
+            'F2RTnso70QpihR1dI'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += ' modal__overlay--visible'
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at agstevens104@gmail.com"
+            );
+        })
+
+}
