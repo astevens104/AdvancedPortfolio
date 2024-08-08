3d12394a6af9ad61771f686e6914494e4308bc49
astevens104 <agstevens104@gmail.com>
2024-08-07 17:56:25 -0700

    modal interactivity complete

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function contact(event) {
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
     loading.classList += " modal__overlay--visible"
-
     emailjs
         .sendForm(
             'service_u7j72lr',
@@ -23,5 +22,14 @@ function contact(event) {
                 "The email service is temporarily unavailable. Please contact me directly at agstevens104@gmail.com"
             );
         })
+}
 
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
